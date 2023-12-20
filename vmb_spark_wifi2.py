44eb11750ef3e38f12c452abaacd80739ab867f1
Zhe Sun <injure21@gmail.com>
2023-12-20 10:40:46 -0500

    modify for vcg push

--- vmb_spark_wifi2.py
+++ vmb_spark_wifi2.py
@@ -24,15 +24,17 @@ if __name__ == "__main__":
     # the only input is the date which is used to generate 'date_range'
     spark = SparkSession.builder.appName('VMB-wifi-score').enableHiveSupport().getOrCreate()
     mail_sender = MailSender() 
-    hdfs_location = "http://njbbvmaspd11.nss.vzwnet.com:9870"
 
     # file ------------------------------------
     hdfs_title = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000'
-    date_str = (date.today() - timedelta(2)).strftime("%Y-%m-%d")
-    path = hdfs_title + "/user/maggie/temp1.1/wifiScore_detail{}.json".format( (date.today() - timedelta(1)).strftime("%Y-%m-%d") )
+    date_str = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
+    #date_str = "2023-12-13"
+    path = hdfs_title + "/user/maggie/temp1.1/wifiScore_detail{}.json".format( date_str )
 
-    models = ['ASK-NCQ1338', 'ASK-NCQ1338FA', 'ASK-NCQ1338E', 'XCI55AX', 'CR1000A', 'FSNO21VA', 'ASK-NCM1100E']
-
+    models_vcg = ['ASK-NCQ1338', 'ASK-NCQ1338FA', 'XCI55AX','CR1000A','WNC-CR200A']
+    models_vbg = ['FSNO21VA','ASK-NCM1100E','ASK-NCQ1338E']
+    models = models_vcg + models_vbg
+    
     df_temp = spark.read.json(path)
     dfsh_all_Hsc = df_temp.groupby("serial_num","mdn","cust_id")\
                                         .agg(
@@ -54,8 +56,12 @@ if __name__ == "__main__":
                          "cust_id"
                          )
     
-    df2=df.join(df_join,"serial_num", "inner")\
-            .selectExpr("to_json(struct(*)) AS value")
+    df2=df.join(df_join,"serial_num", "inner").drop("dg_model")
+    df2.repartition(10).write.mode("overwrite").format("json")\
+            .save( hdfs_title + "/user/ZheS/wifi_score_v2/vcg_vbg/" +  date_str)
+
+    df_vcg = df2.filter( col("dg_model_indiv").isin(models_vcg) )\
+                .selectExpr("to_json(struct(*)) AS value")
 
     pulsar_topic = "persistent://cktv/5g-home-router-wifi-scoring-performance/VMAS-5G-Home-Router-WIFI-Scoring-Performance-daily"
     vmbHost_np = "pulsar+ssl://vmb-aws-us-east-1-nonprod.verizon.com:6651/"
@@ -66,9 +72,9 @@ if __name__ == "__main__":
     capath = key_path + "ca.cert.pem"
 
 
-    #"""
 
-    df2.write.format("pulsar") \
+
+    df_vcg.write.format("pulsar") \
         .option("service.url", vmbHost_np) \
         .option("pulsar.client.authPluginClassName","org.apache.pulsar.client.impl.auth.AuthenticationTls") \
         .option("pulsar.client.authParams",f"tlsCertFile:{cetpath},tlsKeyFile:{keypath}") \
@@ -78,6 +84,7 @@ if __name__ == "__main__":
         .option("pulsar.client.tlsHostnameVerificationenable","false") \
         .option("topic", pulsar_topic) \
         .save()
+
     """
     df2.write.format("pulsar") \
         .option("service.url", vmbHost) \
@@ -91,26 +98,19 @@ if __name__ == "__main__":
         .save()
     """
     #
+    pulsar_topic = "persistent://cktv/5g-home-router-wifi-scoring-performance/VMAS-5G-Home-Router-WIFI-Scoring-Performance-daily"
+    vmbHost_np = "pulsar+ssl://vmb-aws-us-east-1-nonprod.verizon.com:6651/"
+    key_path = "/usr/apps/vmas/cert/cktv/"
 
+    cetpath = key_path + "cktv.cert.pem"
+    keypath = key_path + "cktv.key-pk8.pem"
+    capath = key_path + "ca.cert.pem"
+    job_nonprod = PulsarJob( pulsar_topic ,
+                    vmbHost_np, 
+                    cetpath, 
+                    keypath, 
+                    capath
+                )
+    data = job_nonprod.setup_consumer()
+    mail_sender.send(text = data)
 
-    #-------------------------------------------------------------
-    """
-    combo = "old_wifiscore_topic"
-    if combo == "wifiscore": #does not works
-        pulsar_topic = "persistent://cktv/wifi_score_v2-performance/VMAS-WiFi_score_v2-Performance"
-        vmbHost_np = "pulsar+ssl://vmb-aws-us-west-2-nonprod.verizon.com:6651/"
-        key_path = "/usr/apps/vmas/cert/cktv/wifi_certs/"
-    elif combo == "snap": # works!
-        #pulsar_topic = "persistent://cktv/5g-home-consolidated-performance/VMAS-5G-Home-Consolidated-Performance-Daily"
-        vmbHost_np = "pulsar+ssl://vmb-aws-us-east-1-nonprod.verizon.com:6651/"
-        pulsar_topic = "persistent://cktv/snap_event_enodeb_performance_drop-alert/VMAS-SNAP_event_eNodeB_performance_drop-Alert"
-        key_path = "/usr/apps/vmas/cert/cktv/"
-    elif combo == "wifiscore_oldkey": #does not works
-        pulsar_topic = "persistent://cktv/wifi_score_v2-performance/VMAS-WiFi_score_v2-Performance"
-        vmbHost_np = "pulsar+ssl://vmb-aws-us-west-2-nonprod.verizon.com:6651/"
-        key_path = "/usr/apps/vmas/cert/cktv/"
-    elif combo == "old_wifiscore_topic":
-        pulsar_topic = "persistent://cktv/5g-home-router-wifi-scoring-performance/VMAS-5G-Home-Router-WIFI-Scoring-Performance-daily"
-        vmbHost_np = "pulsar+ssl://vmb-aws-us-east-1-nonprod.verizon.com:6651/"
-        key_path = "/usr/apps/vmas/cert/cktv/"
-    """
