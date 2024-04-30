296cf693f9c28d9cb2c72c47e2669b234377a92e
Zhe Sun <injure21@gmail.com>
2024-04-30 12:51:04 -0400

    Update vmb_spark_wifi2.sh

--- vmb_spark_wifi2.sh
+++ vmb_spark_wifi2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash 
 
-export HADOOP_HOME=/usr/apps/vmas/hadoop-3.2.2 
-export SPARK_HOME=/usr/apps/vmas/spark-3.2.0-bin-hadoop3.2 
+export HADOOP_HOME=/usr/apps/vmas/hadoop-3.3.6
+export SPARK_HOME=/usr/apps/vmas/spark
 export PYSPARK_PYTHON=/usr/apps/vmas/anaconda3/bin/python3 
-echo "RES:" 
+echo "RES: Starting===" 
 echo $(date) 
-/usr/apps/vmas/spark-3.2.0-bin-hadoop3.2/bin/spark-submit \
+/usr/apps/vmas/spark/bin/spark-submit \
 --master spark://njbbepapa1.nss.vzwnet.com:7077 \
 --conf "spark.sql.session.timeZone=UTC" \
 --conf "spark.driver.maxResultSize=2g" \
 --conf "spark.dynamicAllocation.enabled=false" \
---num-executors 10 \
+--num-executors 20 \
 --executor-cores 2 \
---total-executor-cores 20 \
+--total-executor-cores 40 \
 --executor-memory 2g \
---driver-memory 6g \
---packages org.apache.spark:spark-avro_2.12:3.2.0,io.streamnative.connectors:pulsar-spark-connector_2.12:3.2.0.2 \
+--driver-memory 16g \
+--packages org.apache.spark:spark-avro_2.12:3.2.0,io.streamnative.connectors:pulsar-spark-connector_2.12:3.4.0.2 \
 /usr/apps/vmas/script/ZS/spark_vmb/vmb_spark_wifi2.py \
 > /usr/apps/vmas/script/ZS/spark_vmb/vmb_spark_wifi2.log
 
