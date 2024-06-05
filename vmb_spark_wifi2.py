8e714903df0239c97931411828c509a755b260a8
Zhe Sun <injure21@gmail.com>
2024-06-04 20:43:28 -0400

    Update vmb_spark_wifi2.py

--- vmb_spark_wifi2.py
+++ vmb_spark_wifi2.py
@@ -20,6 +20,21 @@ from pyspark.sql.functions import  asc, col, from_json, to_timestamp, window
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType,ArrayType,MapType,TimestampType,LongType
 import pyspark.sql.functions as F
 
+def read_vmb_dataframe(date_str):
+    models_vcg = ['ASK-NCQ1338', 'ASK-NCQ1338FA', 'XCI55AX','CR1000A','WNC-CR200A']
+
+    df_vcg = spark.read.parquet(hdfs_title + f"/user/ZheS/wifi_score_v3/homeScore_dataframe/{date_str}")\
+                .withColumn( "dg_model_indiv", F.explode("dg_model")   )\
+                .withColumn( "dg_model_indiv", F.explode("dg_model_indiv")   )\
+                .select("serial_num",'mdn','cust_id','date','poor_rssi','poor_phyrate',"num_station",'home_score',"dg_model_indiv")\
+                .dropDuplicates()\
+                .fillna({"mdn":"0000000000","cust_id":"000000000"})
+
+    df_vcg = df_vcg.filter( col("dg_model_indiv").isin(models_vcg) )\
+            .selectExpr("to_json(struct(*)) AS value")
+    
+    return df_vcg
+
 if __name__ == "__main__":
     # the only input is the date which is used to generate 'date_range'
     spark = SparkSession.builder.appName('VMB-wifi-score').getOrCreate()
@@ -29,19 +44,7 @@ if __name__ == "__main__":
         hdfs_title = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000'
         date_str = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
 
-        models_vcg = ['ASK-NCQ1338', 'ASK-NCQ1338FA', 'XCI55AX','CR1000A','WNC-CR200A']
-        models_vbg = ['FSNO21VA','ASK-NCM1100E','ASK-NCQ1338E']
-        models = models_vcg + models_vbg
-
-        df2 = spark.read.parquet(hdfs_title + f"/user/ZheS/wifi_score_v3/homeScore_dataframe/{date_str}")
-        df2 = df2.withColumn( "dg_model_indiv", F.explode("dg_model")   )\
-                .withColumn( "dg_model_indiv", F.explode("dg_model_indiv")   )\
-                .select("serial_num",'mdn','cust_id','date','poor_rssi','poor_phyrate',"num_station",'home_score',"dg_model_indiv")\
-                .dropDuplicates()\
-                .fillna({"mdn":"0000000000","cust_id":"000000000"})
-
-        df_vcg = df2.filter( col("dg_model_indiv").isin(models_vcg) )\
-                    .selectExpr("to_json(struct(*)) AS value")
+        df_vcg = read_vmb_dataframe(date_str, sample_num = None)
 
         pulsar_topic = "persistent://cktv/5g-home-router-wifi-scoring-performance/VMAS-5G-Home-Router-WIFI-Scoring-Performance-daily"
         vmbHost_np = "pulsar+ssl://vmb-aws-us-east-1-nonprod.verizon.com:6651/"
@@ -62,13 +65,7 @@ if __name__ == "__main__":
             .option("pulsar.client.tlsHostnameVerificationenable","false")\
             .option("topic", pulsar_topic)\
             .save()
-    except Exception as e:
-        mail_sender.send( send_from ="vmb_spark_wifi@verizon.com", 
-                         text = e,
-                                    subject = f"vmb_spark_wifi prod failed !!! at {date_str}")
-        
-    try:
-        # this is for nonprod, only 10 samples uploaded for test
+
         df_vcg.limit(10)\
             .write.format("pulsar") \
             .option("service.url", vmbHost_np) \
@@ -80,6 +77,7 @@ if __name__ == "__main__":
             .option("pulsar.client.tlsHostnameVerificationenable","false") \
             .option("topic", pulsar_topic) \
             .save()
+
         job_nonprod = PulsarJob( pulsar_topic ,
                                     vmbHost_np, 
                                     cetpath, 
